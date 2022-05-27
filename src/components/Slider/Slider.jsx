a8bf0e86e4284ad705b4046d5bd8b95244645046
Dmitry <ddtihonov@yandex.ru>
2022-05-27 18:51:12 +0300

    доработки слайдера

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,27 +1,22 @@
-import React, {useCallback, useState, useEffect} from 'react';
+import React, {useCallback, useState} from 'react';
 import styles from './Slider.module.scss';
 import { arrSlider } from '../../utils/data';
+import { v4 as uuidv4 } from 'uuid';
 
 export default function Slider () {
 
     const [isCards, setIsCards] = useState(arrSlider);
 
     const handleCardLeft = useCallback(() => {
-        setTimeout(() => {
             let copy = [...isCards];
             copy.push(copy.shift());
             setIsCards(copy);
-    },
-        100)
     }, [isCards]);
 
     const handleCardRight = useCallback(() => {
-        setTimeout(() => {
         let copy = [...isCards];
         copy.unshift(copy.pop());
-        setIsCards(copy);
-        },
-        100)    
+        setIsCards(copy); 
     }, [isCards]);
 
     return (
@@ -29,31 +24,38 @@ export default function Slider () {
             <div className={styles.box}>
                 <h2 className={styles.title}>Участников Академии обучали и консультировали:</h2>
                 <ul className={styles.list}>
-                    <li className={styles.item_small}>
-                        <img className={styles.image_small} src={isCards[0].foto} alt='фото'/>
-                        <h5 className={styles.subtitle_small}>{isCards[0].name}</h5>
-                        <p className={styles.text_small}>{isCards[0].work}</p>
-                    </li>
-                    <li className={styles.item_average}>
-                        <img className={styles.image_average} src={isCards[1].foto} alt='фото'/>
-                        <h5 className={styles.subtitle_average}>{isCards[1].name}</h5>
-                        <p className={styles.text_average}>{isCards[1].work}</p>
-                    </li>
-                        <li className={styles.item}>
-                            <img className={styles.image} src={isCards[2].foto} alt='фото'/>
-                            <h5 className={styles.subtitle}>{isCards[2].name}</h5>
-                            <p className={styles.text}>{isCards[2].work}</p>
-                        </li>
-                <li className={styles.item_average}>
-                    <img className={styles.image_average} src={isCards[3].foto} alt='фото'/>
-                    <h5 className={styles.subtitle_average}>{isCards[3].name}</h5>
-                    <p className={styles.text_average}>{isCards[3].work}</p>
-                </li>
-                <li className={styles.item_small}>
-                    <img className={styles.image_small} src={isCards[4].foto} alt='фото'/>
-                    <h5 className={styles.subtitle_small}>{isCards[4].name}</h5>
-                    <p className={styles.text_small}>{isCards[4].work}</p>
-                </li>
+                    {isCards.map((item, index) => {
+                        const keyUid = uuidv4();
+                        if(index === 0 || index === 4) {
+                            return (
+                                <li className={styles.item_small} key={keyUid}>
+                                    <img className={styles.image_small} src={item.foto} alt='фото'/>
+                                    <h5 className={styles.subtitle_small}>{item.name}</h5>
+                                    <p className={styles.text_small}>{item.work}</p>
+                                </li>
+                            )}
+
+                            if(index === 1 || index === 3) {
+                                return (
+                                    <li className={styles.item_average} key={keyUid}>
+                                        <img className={styles.image_average} src={item.foto} alt='фото'/>
+                                        <h5 className={styles.subtitle_average}>{item.name}</h5>
+                                        <p className={styles.text_average}>{item.work}</p>
+                                    </li>
+                                )   
+                            }
+
+                            if(index === 2) {
+                                return (
+                                    <li className={styles.item} key={keyUid}>
+                                        <img className={styles.image} src={item.foto} alt='фото'/>
+                                        <h5 className={styles.subtitle}>{item.name}</h5>
+                                        <p className={styles.text}>{item.work}</p>
+                                    </li>
+                                )   
+                                
+                            }
+                        })}
                 </ul>
                 <div className={styles.box_button}>
                     <button className={styles.button_left} type="button" onClick={handleCardRight}/>
